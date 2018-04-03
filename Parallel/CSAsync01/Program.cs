bddb609cbaf2d337fd4db978aad851123d9a4d1e
Matrix <matrixislife@outlook.com>
2018-04-03 15:02:09 +0800

    Remove AMP, add C# projects.

---
+++ Parallel/CSAsync01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAsync01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static async Task MyTask()
+        {
+            Console.WriteLine("MyTask is running...");
+            await Task.Delay(2000);
+            Console.WriteLine("MyTask is finished.");
+        }
+    }
+}
---
+++ Parallel/CSThread01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main()
+    {
+        Thread aThread = new Thread(new ParameterizedThreadStart(MyThreadStart));
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} create thread {aThread.ManagedThreadId}.");
+        aThread.Start("Hello, World!");
+        aThread.Join();
+        Console.WriteLine($"Press RETURN to exit thread {Thread.CurrentThread.ManagedThreadId}...");
+        Console.ReadLine();
+    }
+    private static void MyThreadStart(object obj)
+    {
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is running: {obj}");
+    }
+}
